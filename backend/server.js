4ce6e91258f167542b87303afea7b5b2dc446fe1
vinothsubbiah <plvinoth21@gmail.com>
2021-10-07 17:50:37 +0530

    createPost linked with database

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,37 @@ app.get('/api/acc_no',(req, res) =>{
     });
 });
 
+//getting product number
+app.get('/api/prod_no',(req, res) =>{
+    db.query("SELECT COUNT(*) AS count FROM ecommerce.products;", (err, result) =>{
+        res.send(result);
+    });
+});
+
+//add Product 
+app.post('/api/addProd',(req,res) =>{
+    const productNo = req.body.productNo;
+    const productName = req.body.productName;
+    const productPrice = req.body.productPrice;
+    const quantity = req.body.quantity;
+    const categoryNo = req.body.categoryNo;
+    const accountNo = req.body.accountNo;
+    console.log(accountNo);
+    
+    const sqlInsert = "INSERT INTO products (product_no, product_name, product_price, quantity, category_no, account_no) VALUES (?,?,?,?,?,?)";
+
+    db.query(sqlInsert, [productNo, productName, productPrice, quantity, categoryNo, accountNo], (err, result) =>{
+    
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(result);
+        }
+        
+    });
+});
+
 //registration or Sign Up
 app.post('/api/signUp',(req,res) =>{
     const accNo = req.body.accNo;
@@ -64,12 +95,14 @@ app.post('/api/signUp',(req,res) =>{
 //Reset status
 app.get('/api/resetStatus', (req,res) =>{
     status = 0;
+    userAcc = [];
 });
 
 //check status
 app.get('/api/status',(req, res) =>{
     res.send([{status},{userAcc}]);
 });
+
 
 //Log In
 app.post('/api/logIn', (req, res) => {
--- frontend/src/screens/CreatePost.js
+++ frontend/src/screens/CreatePost.js
@@ -1,25 +1,88 @@
 import React, {useState, useEffect} from "react";
-import axios from "axios";
+import Axios from "axios";
+import { useHistory} from "react-router-dom";
 
 function CreatePost(){
+    let [productNo, setProductno] = useState('');
     const [productName, setProductname] = useState('');
     const [productPrice, setProductprice] = useState('');
     const [quantity, setQuantity] = useState('');
+    const [categoryNo, setCategoryno] = useState('');
+    const [accountNo, setAccountno] = useState('');
+
+    let status;
+     const checkStatus = () =>{
+        Axios.get('http://localhost:3001/api/status').then((responce) => {
+        status = responce.data[0].status;
+        //console.log(status);
+        //console.log(responce.data[1].userAcc[0].account_no);
+        setAccountno(responce.data[1].userAcc[0].account_no);
+        if(status!=1){
+            hisLogin();
+        }
+    })
+    };
+    checkStatus();
+
+    const history = useHistory();
+    const hisLogin = () =>{
+        history.push('/logIn');
+    };
+
+    const incProdNo = () =>{
+        Axios.get('http://localhost:3001/api/prod_no').then((responce) => {
+            let num = (responce.data[0].count)+1;
+            setProductno(num);
+            //productNo = productNo +1;
+            console.log(responce.data[0].count+" "+num);
+        });
+        //console.log(accNo);
+        //setAccNo(array[0].count);
+    };
+
+    const submit = () =>{
+        Axios.post('http://localhost:3001/api/addProd', {productNo : productNo, productName : productName, productPrice : productPrice, quantity : quantity, categoryNo : categoryNo, accountNo : accountNo}).then(() => {
+        alert("SignUp Successfull");
+    })
+    };
+    //console.log(categoryNo);
+
     return(
         <div className="cointainer">
+
+
             <div className="form">
+
                     <div className="form-group">
                         <label htmlform="productname" >Product Name </label>
                         <input type="text" name="productname" onChange={(e)=>{
                 setProductname(e.target.value)
             }} required />
                     </div>
+
+
                     <div className="form-group">
                         <label htmlform="productprice" >Product Price </label>
                         <input type="number" name="productprice" onChange={(e)=>{
                 setProductprice(e.target.value)
             }} required />
                     </div>
+
+                    <div className = "form-group">
+                    <label htmlform="category" >Category</label>
+                    <select name="category" onChange={(e)=>{
+                setCategoryno(e.target.value);
+                //console.log(categoryNo);
+                    }}>
+                        <option defaultValue="" ></option>
+                        <option value="1">Electronics</option>
+                        <option value="2">Home Appliances</option>
+                        <option value="3">Pantry</option>
+                        <option value="4">Toys and Games</option>
+                        <option value="5">Sports and Fitness</option>
+                    </select>
+                    </div>
+
                     <div className="form-group">
                         <label htmlform="quantity" >Quantity</label>
                         <input type="number" name="quantity" onChange={(e)=>{
@@ -27,7 +90,11 @@ function CreatePost(){
             }} required />
                     </div>
                     
-                    <button>Create Post</button>
+                    <button onClick={() =>{
+                        submit();
+                        incProdNo();
+                    }}>Create Post</button>
+
                 </div>
         </div>
     );
